0c12b4ad40840d948ce9de3ded74390ef6b62ad4
Philip Coates <phili.coates@epeirogenic.com>
2013-02-11 20:02:29 +0000

    Added PGSQL sequences

--- sql/postgresql/rss_aggregator_schema.sql
+++ sql/postgresql/rss_aggregator_schema.sql
@@ -1,10 +1,23 @@
+CREATE DATABASE rss_aggregator;
+
+-- Sequence: feed_sequence
+
+-- DROP SEQUENCE feed_sequence;
+
+CREATE SEQUENCE feed_sequence
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
 -- Table: feed
 
 -- DROP TABLE feed;
 
 CREATE TABLE feed
 (
-  id bigint NOT NULL,
+  id bigint NOT NULL DEFAULT nextval('feed_sequence'),
   short_name character varying(255) NOT NULL,
   title character varying(255) NOT NULL,
   link character varying(255) NOT NULL,
@@ -17,13 +30,24 @@ CREATE TABLE feed
   CONSTRAINT feed_pkey PRIMARY KEY (id )
 );
 
+-- Sequence: feed_entry_sequence
+
+-- DROP SEQUENCE feed_entry_sequence;
+
+CREATE SEQUENCE feed_entry_sequence
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
 -- Table: feed_entry
 
 -- DROP TABLE feed_entry;
 
 CREATE TABLE feed_entry
 (
-  id bigint NOT NULL,
+  id bigint NOT NULL DEFAULT nextval('feed_entry_sequence'),
   feed_id bigint NOT NULL,
   title character varying(255) NOT NULL,
   link character varying(255) NOT NULL,
@@ -35,13 +59,24 @@ CREATE TABLE feed_entry
   CONSTRAINT feed_entry_pkey PRIMARY KEY (id )
 );
 
+-- Sequence: tag_sequence
+
+-- DROP SEQUENCE tag_sequence;
+
+CREATE SEQUENCE tag_sequence
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
 -- Table: tag
 
 -- DROP TABLE tag;
 
 CREATE TABLE tag
 (
-  id bigint NOT NULL,
+  id bigint NOT NULL DEFAULT nextval('tag_sequence'),
   value character varying(255) NOT NULL,
   CONSTRAINT tag_pkey PRIMARY KEY (id )
 );
